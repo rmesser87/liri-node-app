d0856733018ffe06e7eb3c209a914c0a03b6a9ca
Ross Messer <rmesser@perennial.local>
2018-04-04 16:58:20 -0500

    More js added to liri

--- liri.js
+++ liri.js
@@ -33,7 +33,13 @@ function spotifySearch() {
     spotify
         .search({ type: 'track', query: userData })
         .then(function (response) {
-            console.log(response);
+            // console.log(response.tracks.items);
+            console.log(response.tracks.items[0].name); //song name
+            // console.log(response.tracks.items[0].album.name);//album name
+            // console.log(response.tracks.items[0].artists[0].name); //artists name
+            // console.log(response.tracks.items[0].preview_url);//preview URL
+            
+
         })
         .catch(function (err) {
             console.log(err);
@@ -44,7 +50,7 @@ function omdbSearch() {
     var queryUrl = "http://www.omdbapi.com/?t=" + userData + "&y=&plot=short&apikey=trilogy";
 
     // This line is just to help us debug against the actual URL.
-    console.log(queryUrl);
+    // console.log(queryUrl);
 
     request(queryUrl, function (error, response, body) {
 
@@ -53,7 +59,14 @@ function omdbSearch() {
 
             // Parse the body of the site and recover just the imdbRating
             // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            console.log("Title: " + JSON.parse(body).Title);
             console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].imdbRating);
+            // console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Year);
+            console.log("Production Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
         }
     });
 }
@@ -62,7 +75,9 @@ function twitterSearch() {
     var params = { screen_name: 'rbricem' };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
-            console.log(tweets);
+            for (var i = 0; i < 19; i++) {
+                console.log('Tweet ${i}: ${tweets[i].text} --> was tweeted on ${tweets[i].created_at}');
+            }
         }
     });
 }
