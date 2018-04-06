5bf5ec1d5cfa6d6707b04e3449eabe7d6bace931
Ross Messer <rmesser@perennial.local>
2018-04-05 20:57:21 -0500

    Final changes were made
    clera
    a;lfkjad
    wq

--- liri.js
+++ liri.js
@@ -8,6 +8,8 @@ var request = require("request");
 
 var Spotify = require('node-spotify-api');
 
+var fileSystem = require('file-system')
+
 var spotify = new Spotify(keys.spotify);
 
 var client = new Twitter(keys.twitter);
@@ -19,27 +21,57 @@ var userData = process.argv[3];
 if (command === "my-tweets") {
     twitterSearch();
 } else if (command === "spotify-this-song") {
-    spotifySearch()
+    spotifySearch();
 } else if (command === "movie-this") {
-    omdbSearch()
+    omdbSearch();
 } else if (command === "do-what-it-says") {
-    console.log("arg1 / arg2");
+    randomAct();
+} else if (process.argv.length > 3) {
+    console.log("Please try your command again with the correct parameters.")
+} else {
+    console.log("Please try your command again with the correct parameters.")
 }
 
 
+function randomAct() {
+    fileSystem.readFile("random.txt", "utf8", function (error, data) {
 
+        // 
+        if (error) {
+            return console.log(error);
+        }
+
+        //
+        // console.log(data);
+
+        // 
+        var dataArr = data.split(",");
+
+        // We will then re-display the content as an array for later use.
+        userData = dataArr[1];
+
+        if (dataArr[0] === "my-tweets") {
+            twitterSearch();
+        } else if (dataArr[0] === "spotify-this-song") {
+            spotifySearch();
+        } else if (dataArr[0] === "movie-this") {
+            omdbSearch();
+        }
+        // console.log(dataArr);
+
+    });
+
+}
 
 function spotifySearch() {
     spotify
         .search({ type: 'track', query: userData })
         .then(function (response) {
-            // console.log(response.tracks.items);
-            console.log(response.tracks.items[0].name); //song name
-            // console.log(response.tracks.items[0].album.name);//album name
-            // console.log(response.tracks.items[0].artists[0].name); //artists name
-            // console.log(response.tracks.items[0].preview_url);//preview URL
-            
-
+            var song = response.tracks.items[0]
+            console.log(`The artist you selected is ${song.artists[0].name}.`); //artists name
+            console.log(`The name of the song you selected is ${song.name}.`); //song name
+            console.log(`Here's a link to preview the song ${song.preview_url}.`);//preview URL
+            console.log(`Your song debuted on the album ${song.album.name}.`);//album name
         })
         .catch(function (err) {
             console.log(err);
@@ -59,14 +91,19 @@ function omdbSearch() {
 
             // Parse the body of the site and recover just the imdbRating
             // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-            console.log("Title: " + JSON.parse(body).Title);
-            console.log("Release Year: " + JSON.parse(body).Year);
-            console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].imdbRating);
-            // console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Year);
-            console.log("Production Country: " + JSON.parse(body).Country);
-            console.log("Language: " + JSON.parse(body).Language);
-            console.log("Plot: " + JSON.parse(body).Plot);
-            console.log("Actors: " + JSON.parse(body).Actors);
+            var movie = JSON.parse(body)
+            console.log("Title: " + movie.Title);
+            console.log("Release Year: " + movie.Year);
+            console.log("IMDB Rating: " + movie.Ratings[0].imdbRating);
+            // console.log("Rotten Tomatoes Rating: " + movie.Year);
+            console.log("Production Country: " + movie.Country);
+            console.log("Language: " + movie.Language);
+            console.log("Plot: " + movie.Plot);
+            console.log("Actors: " + movie.Actors);
+        } else {
+            userData = "Mr. Nobody";
+            console.log(error);
+            omdbSearch()
         }
     });
 }
@@ -75,9 +112,16 @@ function twitterSearch() {
     var params = { screen_name: 'rbricem' };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
-            for (var i = 0; i < 19; i++) {
-                console.log('Tweet ${i}: ${tweets[i].text} --> was tweeted on ${tweets[i].created_at}');
+            //I only have 2 tweets at this time. 
+            for (var i = 0; i < 2; i++) {
+                console.log(`Tweet ${i + 1}: ${tweets[i].text} --> was tweeted on ${tweets[i].created_at}`);
             }
         }
     });
 }
+
+// console.log(process.argv[4])
+
+// if (process.argv.length > 3) {
+//     console.log("Please try your command again with the correct parameters.")
+// }
